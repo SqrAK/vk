2f4a8519a9662f7aacde6e7d02e09df3b2394772
Unknown <kapranchickova@yandex.ru>
2018-12-18 16:51:40 +0300

    Add nav to first tab. Add icons

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React, {Component} from 'react';
-import {connect} from 'react-redux';
 import * as UI from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
-// import {isWebView} from '@vkontakte/vkui/src/lib/webview';
-// import * as vkSelectors from '../store/vk/reducer';
-// import * as vkActions from '../store/vk/actions';
 import MainPanel from './containers/Main';
 
 class App extends Component {
 
     componentWillMount() {
-        // this.props.dispatch(vkActions.initApp());
-        // this.props.dispatch(vkActions.fetchAccessToken());
     }
 
     render() {
@@ -28,13 +22,6 @@ class App extends Component {
         );
     }
 }
-
-// function mapStateToProps(state) {
-//     return {
-//         accessToken: vkSelectors.getAccessToken(state),
-//         insets: vkSelectors.getInsets(state),
-//     };
-// }
 
 // export default connect(mapStateToProps)(App);
 export default App;
--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -1,17 +1,20 @@
 import React, {Component} from 'react';
-import {push} from 'react-router-redux';
-import {connect} from 'react-redux';
-import * as UI from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
-import Icon24Notification from '@vkontakte/icons/dist/28/notification';
-import { View, Panel, PanelHeader, TabbarItem,Epic, Tabbar, ListItem } from '@vkontakte/vkui';
+import Icon24Notification from '@vkontakte/icons/dist/24/notification';
+import Icon24Search from '@vkontakte/icons/dist/24/search';
+import Icon24User from '@vkontakte/icons/dist/24/user';
+import { View, Panel, PanelHeader,
+    TabbarItem,Epic, Tabbar, HorizontalScroll,
+    FixedLayout, Tabs, TabsItem,
+    ListItem } from '@vkontakte/vkui';
 
 class Footer extends Component {
     constructor (props) {
         super(props);
 
         this.state = {
-            activeStory: 'more'
+            activeStory: 'more',
+            activeTab6: 'all',
         };
         this.onStoryChange = this.onStoryChange.bind(this);
     }
@@ -39,17 +42,40 @@ class Footer extends Component {
                         onClick={this.onStoryChange}
                         selected={this.state.activeStory === 'notifications'}
                         data-story="notifications"
-                    ><Icon24Notification /></TabbarItem>
+                    ><Icon24Search /></TabbarItem>
                     <TabbarItem
                         onClick={this.onStoryChange}
                         selected={this.state.activeStory === 'more'}
                         data-story="more"
-                    ><Icon24Notification /></TabbarItem>
+                    ><Icon24User /></TabbarItem>
                 </Tabbar>
             }>
                 <View id="feed" activePanel="feed">
                     <Panel id="feed">
-                        <PanelHeader>Feed</PanelHeader>
+                        <FixedLayout vertical="top">
+                            <Tabs theme="header" type="buttons">
+                                <HorizontalScroll>
+                                    <TabsItem
+                                        onClick={() => this.setState({ activeTab6: 'all' })}
+                                        selected={this.state.activeTab6 === 'all'}
+                                    >
+                                        Выбор редакции
+                                    </TabsItem>
+                                    <TabsItem
+                                        onClick={() => this.setState({ activeTab6: 'users' })}
+                                        selected={this.state.activeTab6 === 'users'}
+                                    >
+                                        Новинки
+                                    </TabsItem>
+                                    <TabsItem
+                                        onClick={() => this.setState({ activeTab6: 'groups' })}
+                                        selected={this.state.activeTab6 === 'groups'}
+                                    >
+                                        Популярные
+                                    </TabsItem>
+                                </HorizontalScroll>
+                            </Tabs>
+                        </FixedLayout>
                     </Panel>
                 </View>
                 <View id="discover" activePanel="discover">
